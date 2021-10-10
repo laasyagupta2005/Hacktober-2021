c57616721d48deebf18bca0d954911d065c94caa
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-10 18:36:10 +0530

    added linear search in c++

---
+++ C++/linearSearch.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    //taking the size of array
+    int n;
+    cout<<"enter the size of array:";
+    cin>>n;
+    int arr[n];
+
+    //inputing the data
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter element "<<i+1<<":";
+        cin>>arr[i];
+    }
+    //asking for key
+    int key;
+    cout<<"enter element to find:";
+    cin>>key;
+
+    //searching into array
+    int pos = -1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            pos = i+1;
+            break;
+        }
+    }
+    if(pos==-1)
+        cout<<"element not found\n";
+    else
+        cout<<"element found at position:"<<pos<<endl;
+
+    return 0;
+
+}
